2fd2a47444c05b722926a47f764f68bc6eacc5ef
Julia-fly <uliasergeeva3896@gmail.com>
2024-10-23 19:25:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print(12+89-1)
+#1program
+print(9**0.5*5)
+#2program
+print(9.99>9.98 and 1000!=1000.1)
+#3program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+#4program
+print(123.456*10)
