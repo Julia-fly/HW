167dfeb8dba79852605c9e796634216ea67fde7f
Julia-fly <uliasergeeva3896@gmail.com>
2024-10-23 20:44:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-print(12+89-1)
-#1program
-print(9**0.5*5)
-#2program
-print(9.99>9.98 and 1000!=1000.1)
-#3program
-print(2*2+2)
-print(2*(2+2))
-print((2*2+2)==(2*(2+2)))
-#4program
-print(123.456*10)
+#1Task
+name = 'Python'
+HW_counds = 12
+hours = 1.5
+time_for_one_HW = (HW_counds / hours)
+print(name,'всего задач:', HW_counds, 'затрачено часов:', hours, 'среднее время выполнения', time_for_one_HW, 'часа.')
+#2Task
+name1 = ('грузовика')
+name2 = ('легковые машины')
+First_car =2
+Second_car =4
+total_car = (First_car+Second_car)
+print('В гараже',First_car, name1, 'и', Second_car, name2, 'всего в гараже', total_car, 'машин')
+#3Task
+pax = 5
+price = 50
+price_ticket = (50//5)
+name = 'пассажиров'
+car = 'автобусе'
+print(pax,name,'заплатили за проезд в ', car, price,'р.','стоимость проезда за одного пассажира', price_ticket )
+#4task
+name = 'апельсин'
+who = 'мы'
+verb = 'делили'
+quantity_people = 4
+quantity_orange = 1
+counds_orange = quantity_orange/quantity_people
+print(who,verb,name, 'нас',quantity_people, 'он', quantity_orange,'.', 'нам досталось',counds_orange, 'апельсина.')
